37e2f01d63907961cc1927df57ea7914aa4fc079
k4ifman <ruslan.steblin2001@gmail.com>
2026-09-16 10:27:50 +0300

    fix: compile lambdas with JDK platform before Android D8

--- gardenpilot/build.sh
+++ gardenpilot/build.sh
@@ -6,12 +6,12 @@ if [[ -z "$GP_SDK" ]]; then echo "Set ANDROID_HOME to an Android SDK installatio
 GP_BT="$GP_SDK/build-tools/35.0.0"
 GP_JAR="$GP_SDK/platforms/android-35/android.jar"
 mkdir -p build/classes build/generated build/dex build/tests dist
-javac -encoding UTF-8 -source 8 -target 8 -d build/tests src/app/gardenpilot/Engine.java tests/EngineTest.java
+javac -encoding UTF-8 --release 8 -d build/tests src/app/gardenpilot/Engine.java tests/EngineTest.java
 java -ea -cp build/tests app.gardenpilot.EngineTest | tee build/test-results.txt
 "$GP_BT/aapt2" compile --dir res -o build/resources.zip
 "$GP_BT/aapt2" link -o build/resources.apk -I "$GP_JAR" --manifest AndroidManifest.xml --java build/generated --min-sdk-version 34 --target-sdk-version 35 build/resources.zip
 find src build/generated -name '*.java' -print > build/sources.txt
-javac -encoding UTF-8 -source 8 -target 8 -bootclasspath "$GP_JAR" -d build/classes @build/sources.txt
+javac -encoding UTF-8 --release 8 -classpath "$GP_JAR" -d build/classes @build/sources.txt
 jar cf build/classes.jar -C build/classes .
 "$GP_BT/d8" --release --min-api 34 --lib "$GP_JAR" --output build/dex build/classes.jar
 cp build/resources.apk build/unsigned.apk
